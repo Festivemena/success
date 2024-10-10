c1f1a26666eda977abc2273730f1dd818234f138
Festivemena <festusmena9@gmail.com>
2024-10-10 22:33:16 +0100

    modified:   data/index.ts

--- data/index.ts
+++ data/index.ts
@@ -107,31 +107,31 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
+      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success' enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
+      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success' enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
+      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success' enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
+      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success' enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
+      "Collaborating with Success was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Success' enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Success is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
