4594d8e59a7c132ae6f5f2d41e92d840a5209f33
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-09-17 12:29:19 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mr Success",
+  title: "OxF3stus",
   description: "Hardware Engineer",
 };
 
