f3f3300386da91c408c85b007ca3294251eec7f7
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-09-17 12:16:53 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-         Hi! I'm Success, a Hardware Engineer based in Nigeria.</p>
+         Hi! I am Efemena, a Hardware Engineer based in Nigeria.</p>
 
           <a href="#about">
             <MagicButton
